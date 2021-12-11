54d92486f5a84ab4230de0a917ea90d2b996b2b1
Jackson Oppenheim <jacksonop@gmail.com>
2021-12-10 21:30:02 -0600

    added save game

--- GameStats.gd
+++ GameStats.gd
@@ -1,12 +1,22 @@
 extends Node
 
 var score := 0 setget set_score
+var savedata := {"highscore": 0}
 var is_game_started = false setget set_is_game_started
 var is_game_over = true setget set_is_game_over
+
+var savegame = File.new()
+var save_path = "user://savegame.dat"
 
 signal score_change(value)
 signal game_over(value)
 signal game_start
+
+func _ready() -> void:
+	var error = savegame.open(save_path, File.READ)
+	if error != OK:
+		create_save()
+	savegame.close()
 
 func _input(event: InputEvent) -> void:
 	if !is_game_started and !is_game_over:
@@ -20,6 +30,7 @@ func set_score(value: int):
 func set_is_game_started(value: bool) -> void:
 	is_game_started = value
 	if value:
+		score = 0
 		emit_signal("game_start")
 	
 func set_is_game_over(value: bool) -> void:
@@ -27,3 +38,20 @@ func set_is_game_over(value: bool) -> void:
 	if value:
 		is_game_started = false
 		emit_signal("game_over", score)
+
+func create_save() -> void:
+	savegame.open(save_path, File.WRITE)
+	savegame.store_var(savedata)
+	savegame.close()
+	
+func save(high_score: int) -> void:
+	savedata["highscore"] = high_score
+	savegame.open(save_path, File.WRITE)
+	savegame.store_var(savedata)
+	savegame.close()
+
+func read_savegame() -> Dictionary:
+	savegame.open(save_path, File.READ)
+	savedata = savegame.get_var()
+	savegame.close()
+	return savedata
--- UI/Interface.gd
+++ UI/Interface.gd
@@ -5,15 +5,18 @@ var state = GameStats
 onready var gameOverScreen = $GameOverScreen
 onready var startGameButton = $GameOverScreen/ButtonArea/StartGameButton
 onready var scoreLabel = $Score/ScoreLabel
+onready var highScore = $GameOverScreen/HighScoreArea/HighScore
 onready var yourScore = $GameOverScreen/HighScoreArea/YourScore
 onready var pressSpace = $PressSpace
 
 func _ready() -> void:
 	state.connect("game_over", self, "show_game_over_screen")
 	state.connect("score_change", self, "update_score")
+	show_game_over_screen(0)
 	
 func _process(delta: float) -> void:
 	if !state.is_game_over and !state.is_game_started:
+		update_score(0)
 		pressSpace.show()
 	else:
 		pressSpace.hide()
@@ -24,6 +27,13 @@ func _on_StartGameButton_pressed() -> void:
 	gameOverScreen.hide()
 	
 func show_game_over_screen(score: int) -> void:
+	var prev_high_score = state.read_savegame()
+	if score > prev_high_score.highscore:
+		state.save(score)
+		highScore.text = str(score)
+	else:
+		var high_score = state.read_savegame()
+		highScore.text = str(high_score.highscore)
 	yourScore.text = str(score)
 	gameOverScreen.show()
 
